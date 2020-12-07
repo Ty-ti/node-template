14fba8174738bcbcc4269673af0b54293c7fdf97
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2020-12-06 23:01:03 -0300

    Update module-alias.ts

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
-import moduleAlias from 'module-alias';
+import moduleAlias 
+                     from 'module-alias';
 
 const files = path.resolve(__dirname, '../..');
 
