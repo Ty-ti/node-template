3fd5d6916b8bc1d1fbbb362445ced2c9cc5ad24d
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2020-12-08 09:08:19 -0300

    Update globals.d.ts

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,4 +1,5 @@
-declare namespace NodeJS {
+declare namespace 
+  NodeJS {
   interface Global {
     //https://stackoverflow.com/a/51114250
     testRequest: import('supertest').SuperTest<import('supertest').Test>;
